1b614680f888c5ad347acbceed9d2be75f61cb16
tbgamal <tbgamalm@gmail.com>
2023-11-08 13:03:12 -0600

    committing progress after successfully fetching API and handling submit into the form

---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react"
+
+function SignUpForm () {
+
+  const [username, setUsername] = useState ("")
+  const [password, setPassword] = useState ("")
+  const [error, setError] = useState (null)
+
+  async function handleSubmit (event) {
+    event.preventDefault();
+    // console.log ("Hello");
+
+    try{
+      const response = await fetch ('https://fsa-jwt-practice.herokuapp.com/signup', {
+        method: 'POST',
+        headers: {
+          "Content-Type" : "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          password
+        })
+      })
+      const result = await response.json();
+      console.log (result)
+    }
+    catch(err){
+      setError(err.message)
+    }
+
+  }
+
+  return (
+    <>
+      <h2>Sign Up</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
+        </label>
+        <label>
+          Password:
+          <input type="text" value={password} onChange={(e) => setPassword(e.target.value)}/>
+        </label>
+        <button>Sign Up</button>
+      </form>    
+    </>
+  )
+}
+
+export default SignUpForm
